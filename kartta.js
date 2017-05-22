5bbb5313ee490efb40d2c0f9be944703401b190d
matleppa <matti.leppanen@apinf.io>
2017-05-22 12:02:26 +0300

    Added function for a map

---
+++ kartta.js
@@ -0,0 +1,33 @@
+function turku_map() {
+
+  //console.log('yritetään tehdä karttaa etusivulle ');
+  //<!-- Add Google Maps -->
+  var myCenter = new google.maps.LatLng(61.5287, 23.63215);
+  //console.log('karttaolio luotu ' );
+
+  function initialize() {
+    //console.log('initializessa ' );
+    var mapProp = {
+                   center:      myCenter,
+                   zoom:        12,
+                   scrollwheel: true,
+                   draggable:   false,
+                   mapTypeId:   google.maps.MapTypeId.ROADMAP
+  };
+
+    //console.log('mapProp ', mapProp );
+
+    var map = new google.maps.Map(document.getElementById('mapPoint'),mapProp);
+
+    var marker = new google.maps.Marker({
+      position:myCenter,
+    });
+
+    marker.setMap(map);
+  }
+
+  //google.maps.event.addDomListener(window, 'load', initialize);
+  //console.log('kutsutaan initializea ' );
+  initialize();
+
+}
--- turku_api_demo.js
+++ turku_api_demo.js
@@ -1,8 +1,9 @@
 var sourceURL = "https://apinf.io:3002/turku_feedback_v1/services.json"
-var key = "oJOTXpoZNwDvr9cswtVhvPencYWZK58bXe6XiDTO"
+var key = ""
 
 $(document).ready(function() {
   $.getJSON(sourceURL + "?api_key=" + key, function(items) {
     $('#content').append("<h2>Found again " + items.length + " items</h2>");
   });
+  turku_map();
 });

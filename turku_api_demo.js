a15ee7be57e652f0e8fdb3efc8094e15af065456
wuder4 <Joonas.heikkila@samposoftware.com>
2017-05-22 15:22:33 +0300

    parsing started

--- turku_api_demo.js
+++ turku_api_demo.js
@@ -4,6 +4,7 @@ var key = ""
 $(document).ready(function() {
   $.getJSON(sourceURL + "?api_key=" + key, function(items) {
     $('#content').append("<h2>Found again " + JSON.stringify(items) + " items</h2>");
-  
+  //String serviceNameStr = JSON.stringify(items).getString("service_name");
+                
   });
 });

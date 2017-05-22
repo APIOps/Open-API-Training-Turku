bba99ad34f315cec07d3c8663056ca835e23c2b4
wuder4 <Joonas.heikkila@samposoftware.com>
2017-05-22 15:00:08 +0300

    Url fixed

--- turku_api_demo.js
+++ turku_api_demo.js
@@ -1,4 +1,4 @@
-var sourceURL = "https://api.turku.fi/feedback/v1/requests.json"
+var sourceURL = "https://api.turku.fi/feedback/v1/requests.json?start_date=2017-05-01T00:00:00Z&end_date=2017-05-21T00:00:00Z"
 var key = ""
 
 $(document).ready(function() {

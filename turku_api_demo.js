0b1e4ca446d7efdbbd0b7b50dcebecd12d99f935
matleppa <matti.leppanen@apinf.io>
2017-05-23 08:06:21 +0300

    Added initializing parameter map

--- turku_api_demo.js
+++ turku_api_demo.js
@@ -5,13 +5,20 @@ $(document).ready(function() {
   $.getJSON(sourceURL + "?api_key=" + key, function(items) {
     let lista = JSON.stringify(items);
     let luettelo;
+
+    var turku = {lat: 60.4518126, lng: 22.266630299999974};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: turku
+    });
+
     items.forEach(function(tieto) {
       $('#content').append(tieto.service_name+' '+tieto.address+' lat='+tieto.lat+' lon='+tieto.long+'<br>');
       // drawMap(tieto.lat, tieto.long);
       // luettelo = luettelo + tieto +"<br>";
       if (tieto.lat && tieto.long) {
         var turku = {lat: tieto.lat, lng: tieto.long};
-        addMarker(turku);
+        addMarker(turku, map);
       }
     });
     $('#content').append(lista);
